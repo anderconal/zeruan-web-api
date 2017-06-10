8b423ae86118d72257d1d95228e099e91a72cfb7
Ander Conal <ander.conal@jinnapp.com>
2017-06-10 12:04:09 +0200

    Create Client model Applying TDD: 3 tests created for the Client model Client model created Added Client model to the admin.py

--- zeruanwebapi/clients/models.py
+++ zeruanwebapi/clients/models.py
@@ -2,5 +2,95 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from multiselectfield import MultiSelectField
+from model_utils import Choices
 
-# Create your models here.
+PARTNER_OPTIONS = Choices(
+    ('NO_PARTNER', 'No socio'),
+    ('PARTNER_A', 'Tipo A'),
+    ('PARTNER_B', 'Tipo B'),
+    ('PARTNER_C', 'Tipo C')
+)
+
+KNOWN_FOR_CHOICES = Choices(
+    ('FACEBOOK', 'Facebook'),
+    ('INSTAGRAM', 'Instagram'),
+    ('INTERNET', 'Internet'),
+    ('RADIO', 'Radio'),
+    ('BOCA_A_BOCA', 'Boca a boca'),
+    ('LOCALIZACION', 'Localización'),
+    ('PUBLICIDAD_IMPRESA', 'Publicidad impresa'),
+    ('OTROS', 'Otros')
+)
+
+LOPD_CHANNEL_CHOICES = Choices(
+    ('WHATSAPP', 'WhatsApp'),
+    ('EMAIL', 'Email'),
+    ('NONE', 'No LOPD')
+)
+
+LOPD_OPTION_CHOICES = Choices(
+    ('FOTODEPILACION', 'Fotodepilación'),
+    ('MANICURA', 'Manicura'),
+    ('PEDICURA', 'Pedicura'),
+    ('MAQUILLAJE', 'Maquillaje'),
+    ('TRATAMIENTOS_FACIALES', 'Tratamientos faciales'),
+    ('TRATAMIENTOS_CORPORALES', 'Tratamientos corporales'),
+    ('CEJAS_Y_PESTANAS', 'Cejas y pestañas')
+)
+
+
+class Client(models.Model):
+    """ Client model. """
+    dni = models.CharField(max_length=9, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    second_surname = models.CharField(max_length=255, blank=True, null=True)
+    birthdate = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True,
+        null=True)
+    phone_number = models.CharField(max_length=21)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=5, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    province = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    release_date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True,
+        null=True)
+    partner = models.CharField(
+        choices=PARTNER_OPTIONS,
+        default=PARTNER_OPTIONS.NO_PARTNER,
+        max_length=255
+    )
+    partner_release_date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True, null=True
+    )
+    known_for = models.CharField(
+        choices=KNOWN_FOR_CHOICES,
+        default=KNOWN_FOR_CHOICES.FACEBOOK,
+        max_length=255
+    )
+    lopd = models.BooleanField(default=False)
+    lopd_channel = models.CharField(
+        choices=LOPD_CHANNEL_CHOICES,
+        default=LOPD_CHANNEL_CHOICES.NONE,
+        max_length=255
+    )
+    lopd_options = MultiSelectField(
+        choices=LOPD_OPTION_CHOICES,
+        default=None,
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name + ' ' + self.surname + ' ' + self.second_surname
--- zeruanwebapi/clients/tests.py
+++ zeruanwebapi/clients/tests.py
@@ -1,6 +1,76 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.test import TestCase
+from .models import Client
+from .models import PARTNER_OPTIONS, KNOWN_FOR_CHOICES, LOPD_CHANNEL_CHOICES, LOPD_OPTION_CHOICES
 
-# Create your tests here.
+
+class ClientTestCase(TestCase):
+    """This class defines the test suite for the Client model."""
+
+
+    def setUp(self):
+        """Define the test Client and other test variables."""
+        self.test_client = Client.objects.create(
+            dni='12345678t',
+            name='Ander',
+            surname='Conal',
+            second_surname='Fuertes',
+            birthdate=datetime.now(),
+            phone_number='666666666',
+            address='Fake Street, 7',
+            postal_code='66666',
+            city='Bilbao',
+            province='Vizcaya',
+            email='ander.conal@gmail.com',
+            release_date=datetime.now(),
+            partner=PARTNER_OPTIONS.NO_PARTNER,
+            partner_release_date=datetime.now(),
+            known_for=KNOWN_FOR_CHOICES.FACEBOOK,
+            lopd=True,
+            lopd_channel=LOPD_CHANNEL_CHOICES.WHATSAPP,
+            lopd_options=LOPD_OPTION_CHOICES.FOTODEPILACION,
+            notes='Test'
+        )
+
+
+    def test_string_representation(self):
+        """String representation of Client model should be: name (white space) surname (white space) second_surname"""
+
+        self.assertEqual(str(self.test_client), self.test_client.name + ' ' +
+                         self.test_client.surname + ' ' +
+                         self.test_client.second_surname)
+
+
+    def test_verbose_name_plural(self):
+        """The pluralization of Client should be Clients"""
+        self.assertEqual(str(Client._meta.verbose_name_plural), 'clients')
+
+
+    def test_model_can_create_a_client(self):
+        """Test the Client model can create a Client."""
+        old_count = Client.objects.count()
+        Client.objects.create(
+            dni='87654321t',
+            name='Pepe',
+            surname='Test',
+            second_surname='Django',
+            birthdate=datetime.now(),
+            phone_number='999999999',
+            address='Fake Street, 9',
+            postal_code='99999',
+            city='Bilbao',
+            province='Vizcaya',
+            email='test@gmail.com',
+            release_date=datetime.now(),
+            partner=PARTNER_OPTIONS.NO_PARTNER,
+            partner_release_date=datetime.now(),
+            known_for=KNOWN_FOR_CHOICES.FACEBOOK,
+            lopd=True,
+            lopd_channel=LOPD_CHANNEL_CHOICES.WHATSAPP,
+            lopd_options=LOPD_OPTION_CHOICES.FOTODEPILACION,
+            notes='Test'
+        )
+        new_count = Client.objects.count()
+        self.assertNotEqual(old_count, new_count)
