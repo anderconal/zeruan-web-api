b44abeddf7f760182ac0de4fe774ae441b692ae9
Ander Conal <ander.conal@jinnapp.com>
2017-06-13 23:00:02 +0200

    GET, PUT and DELETE requests for the Client model Needed Unit Tests added

--- zeruanwebapi/clients/tests.py
+++ zeruanwebapi/clients/tests.py
@@ -116,3 +116,54 @@ class ClientViewTestCase(TestCase):
     def test_can_create_a_client(self):
         """Test the API has Client creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_can_get_a_client(self):
+        """Test the API can get a given Client."""
+        client = Client.objects.get()
+        response = self.client.get(
+            reverse('details', kwargs={'pk': client.id}),
+            format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, client.id)
+
+
+    def test_can_update_a_client(self):
+        """Test the API can update a given Client."""
+        client = Client.objects.get()
+        client_to_update = {
+            "dni": "12333333a",
+            "name": "Pepe",
+            "surname": "Yo",
+            "second_surname": "",
+            "birthdate": "1992-06-22",
+            "phone_number": "999323456",
+            "address": "Tellafake, 2",
+            "postal_code": "48012",
+            "city": "London",
+            "province": "Vizcaya",
+            "email": "harry@potter.es",
+            "release_date": "2017-06-11",
+            "partner": "NO_PARTNER",
+            "partner_release_date": None,
+            "known_for": "FACEBOOK",
+            "lopd": False,
+            "lopd_channel": "WHATSAPP",
+            "lopd_options": "",
+            "notes": ""
+        }
+        response = self.client.put(
+            reverse('details', kwargs={'pk': client.id}),
+            client_to_update, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_can_delete_a_client(self):
+        """Test the API can delete a Client."""
+        client = Client.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': client.id}),
+            format='json',
+            follow=True)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
