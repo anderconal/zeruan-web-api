23ac526e92ca7cd31b90638958ac1cf0012d40ec
Ander Conal <ander.conal@jinnapp.com>
2017-07-26 14:08:46 +0200

    Final updates on InvoiceDetail model Unit tests finished on both model and API. Little code refactor.

--- zeruanwebapi/invoice_details/tests.py
+++ zeruanwebapi/invoice_details/tests.py
@@ -81,11 +81,11 @@ class InvoiceDetailModelTestCase(TestCase):
         self.assertEqual(str(InvoiceDetail._meta.verbose_name_plural), 'invoice details')
 
 
-    def test_model_can_create_a_invoice_detail(self):
-        """Test the InvoiceDetail model can create a InvoiceDetail."""
+    def test_model_can_create_a_invoice_detail_with_all_the_defaults(self):
+        """Test the InvoiceDetail model can create a InvoiceDetail with all the defaults."""
         old_count = InvoiceDetail.objects.count()
 
-        InvoiceDetail.objects.create(
+        test_invoice_detail = InvoiceDetail.objects.create(
             invoice=self.test_invoice,
             service=self.test_service
         )
@@ -93,6 +93,10 @@ class InvoiceDetailModelTestCase(TestCase):
         new_count = InvoiceDetail.objects.count()
 
         self.assertNotEqual(old_count, new_count)
+        self.assertEqual(test_invoice_detail.quantity, 1)
+        self.assertEqual(test_invoice_detail.vat, VAT_CHOICES.GENERAL)
+        self.assertEqual(test_invoice_detail.payment_method, PAYMENT_METHODS.CASH)
+        self.assertEqual(test_invoice_detail.discount, 0)
 
 
 class InvoiceDetailViewTestCase(TestCase):
@@ -161,15 +165,97 @@ class InvoiceDetailViewTestCase(TestCase):
         )
 
 
-    def test_api_can_create_a_invoice_detail(self):
-        """Test the API has InvoiceDetail creation capability."""
+    def test_api_can_create_a_invoice_detail_with_a_service(self):
+        """Test the API has InvoiceDetail creation capability (with a service)."""
+        invoice_detail_with_a_service = {
+            "invoice": self.test_invoice.id,
+            "service": self.test_service.id
+        }
+
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            invoice_detail_with_a_service,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        InvoiceDetail.objects.get(pk=response.json().get('id'))
+
+
+    def test_api_can_create_a_invoice_detail_with_a_product(self):
+        """Test the API has InvoiceDetail creation capability (with a product)."""
+        invoice_detail_with_a_product = {
+            "invoice": self.test_invoice.id,
+            "product": self.test_product.id
+        }
+
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            invoice_detail_with_a_product,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        InvoiceDetail.objects.get(pk=response.json().get('id'))
+
+
+    def test_api_can_create_a_invoice_detail_paid_with_a_prepaid_card(self):
+        """Test the API has InvoiceDetail creation capability (paid with a prepaid card)."""
+        invoice_detail_with_a_product = {
+            "invoice": self.test_invoice.id,
+            "product": self.test_product.id,
+            "prepaid_card": self.test_prepaid_card.id
+        }
+
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            invoice_detail_with_a_product,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        InvoiceDetail.objects.get(pk=response.json().get('id'))
+
+
+    def test_default_quantity(self):
+        """The default quantity should be 1."""
         response = self.api_client.post(
             reverse('invoice-detail-create'),
             self.invoice_detail_data,
             format="json")
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        InvoiceDetail.objects.get(pk=response.json().get('id'))
+        self.assertEqual(response.json().get('quantity'), 1)
+
+
+    def test_default_vat(self):
+        """The default vat should be GENERAL."""
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            self.invoice_detail_data,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json().get('vat'), VAT_CHOICES.GENERAL)
+
+
+    def test_default_discount(self):
+        """The default discount should be 0."""
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            self.invoice_detail_data,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json().get('discount'), 0)
+
+
+    def test_default_payment_method(self):
+        """The default payment method should be CASH."""
+        response = self.api_client.post(
+            reverse('invoice-detail-create'),
+            self.invoice_detail_data,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json().get('payment_method'), PAYMENT_METHODS.CASH)
 
 
     def test_api_can_get_a_invoice_detail(self):
